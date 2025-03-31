ef1c420b7cdac94ea8a78e589c489bd84f2a357a
Thiago Tavares <thiago.tlopess@gmail.com>
2025-03-30 21:49:09 -0300

    att

---
+++ script1.R
@@ -0,0 +1,10 @@
+library(readr)
+german_credit_data <- read_csv("german_credit_data.csv")
+View(german_credit_data)
+library(arrow)
+
+
+# Exemplo: lendo um CSV
+# creditcard<- read_csv("creditcard.csv")
+# 
+# write_parquet(creditcard, "creditcard.parquet")
